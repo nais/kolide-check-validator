582c9c0f72c62dd283350cd3b504f3200f64be52
Christer Edvartsen <christer.edvartsen@nav.no>
2024-03-22 16:15:34 +0100

    move main logic to separate package

--- cmd/kolide-check-validator/main.go
+++ cmd/kolide-check-validator/main.go
@@ -2,14 +2,9 @@ package main
 
 import (
 	"context"
-	"fmt"
-	"net/http"
 	"os"
-	"time"
 
-	"github.com/hashicorp/go-retryablehttp"
-	kac "github.com/navikt/kolide-check-validator/internal/kolide-api-client"
-	sc "github.com/navikt/kolide-check-validator/internal/slack-client"
+	"github.com/navikt/kolide-check-validator/internal/cmd/kolide-check-validator"
 	"github.com/sirupsen/logrus"
 )
 
@@ -20,63 +15,12 @@ const (
 
 func main() {
 	ctx := context.Background()
-
 	log := logrus.StandardLogger()
-	if err := run(ctx, log); err != nil {
+
+	if err := kolide_check_validator.Run(ctx, log); err != nil {
 		log.WithError(err).Errorf("error in run: %v", err)
 		os.Exit(exitRunError)
 	}
 
 	os.Exit(exitSuccess)
-}
-
-func run(ctx context.Context, log logrus.FieldLogger) error {
-	ctx, cancel := context.WithCancel(ctx)
-	defer cancel()
-
-	kolideApiToken := os.Getenv("KOLIDE_API_TOKEN")
-	slackWebhook := os.Getenv("SLACK_WEBHOOK")
-
-	kolideApiClient := kac.New(getHttpClient(log), kolideApiToken, log.WithField("client", "Kolide"))
-	slackClient := sc.New(getHttpClient(log), slackWebhook, log.WithField("client", "Slack"))
-
-	log.Infof("validate Kolide checks")
-	timeout, cancel := context.WithTimeout(ctx, 1*time.Minute)
-	checks, err := kolideApiClient.GetChecks(timeout)
-	cancel()
-
-	if err != nil {
-		return fmt.Errorf("get Kolide checks: %w", err)
-	}
-
-	incompleteChecks := make([]kac.Check, 0)
-	for _, check := range checks {
-		if !check.HasSeverityTag() {
-			incompleteChecks = append(incompleteChecks, check)
-		}
-	}
-
-	log.
-		WithField("num_checks", len(checks)).
-		WithField("num_incomplete_checks", len(incompleteChecks)).
-		Infof("validated Kolide checks")
-
-	if len(incompleteChecks) > 0 {
-		timeout, cancel = context.WithTimeout(ctx, 1*time.Minute)
-		err = slackClient.Notify(timeout, incompleteChecks)
-		cancel()
-
-		if err != nil {
-			return fmt.Errorf("notify Slack: %w", err)
-		}
-	}
-
-	return nil
-}
-
-func getHttpClient(log logrus.FieldLogger) *http.Client {
-	retryableClient := retryablehttp.NewClient()
-	retryableClient.Logger = log
-	retryableClient.RetryMax = 10
-	return retryableClient.StandardClient()
 }
---
+++ internal/cmd/kolide-check-validator/kolide-check-validator.go
@@ -0,0 +1,65 @@
+package kolide_check_validator
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/hashicorp/go-retryablehttp"
+	kac "github.com/navikt/kolide-check-validator/internal/kolide-api-client"
+	sc "github.com/navikt/kolide-check-validator/internal/slack-client"
+	"github.com/sirupsen/logrus"
+)
+
+func Run(ctx context.Context, log logrus.FieldLogger) error {
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	kolideApiToken := os.Getenv("KOLIDE_API_TOKEN")
+	slackWebhook := os.Getenv("SLACK_WEBHOOK")
+
+	kolideApiClient := kac.New(getHttpClient(log), kolideApiToken, log.WithField("client", "Kolide"))
+	slackClient := sc.New(getHttpClient(log), slackWebhook, log.WithField("client", "Slack"))
+
+	log.Infof("validate Kolide checks")
+	timeout, cancel := context.WithTimeout(ctx, 1*time.Minute)
+	checks, err := kolideApiClient.GetChecks(timeout)
+	cancel()
+
+	if err != nil {
+		return fmt.Errorf("get Kolide checks: %w", err)
+	}
+
+	incompleteChecks := make([]kac.Check, 0)
+	for _, check := range checks {
+		if !check.HasSeverityTag() {
+			incompleteChecks = append(incompleteChecks, check)
+		}
+	}
+
+	log.
+		WithField("num_checks", len(checks)).
+		WithField("num_incomplete_checks", len(incompleteChecks)).
+		Infof("validated Kolide checks")
+
+	if len(incompleteChecks) > 0 {
+		timeout, cancel = context.WithTimeout(ctx, 1*time.Minute)
+		err = slackClient.Notify(timeout, incompleteChecks)
+		cancel()
+
+		if err != nil {
+			return fmt.Errorf("notify Slack: %w", err)
+		}
+	}
+
+	return nil
+}
+
+func getHttpClient(log logrus.FieldLogger) *http.Client {
+	retryableClient := retryablehttp.NewClient()
+	retryableClient.Logger = log
+	retryableClient.RetryMax = 10
+	return retryableClient.StandardClient()
+}
